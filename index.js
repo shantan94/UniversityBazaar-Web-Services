d3e908cb38c0806acb993fb6d2888210bba4a38b
shantan94 <shantanreddy94@gmail.com>
2018-02-23 09:40:39 -0600

    added items service

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ app.post('/users/items',function(req,res){
     let image=req.body.image;
     let type=req.body.type;
     let price=req.body.price;
-    let query=`insert into items values('${userid}','${items}','${description}',${image},'${type}','${price}')`;
+    let query=`insert into items values('${userid}','${itemname}','${description}',${image},'${type}','${price}')`;
     connection.query(query,function(error,results,fields){
         if(error){
             res.send({error:true,status:400,message:'Failed'});
